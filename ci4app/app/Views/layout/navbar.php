c282e7aeb64cee43d6ab511db795ba7cf948dde3
ilhamfakihhilmi <67012067+ilhamfakihhilmi@users.noreply.github.com>
2020-12-23 00:28:22 +0700

    update home page
    perbaikan section

--- ci4app/app/Views/layout/navbar.php
+++ ci4app/app/Views/layout/navbar.php
@@ -1,17 +1,17 @@
-<nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container">
-        <h3 class="logo">Teknow<span>logue</span></h3>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" href="/">Home</a>
-                <a class="nav-link" href="/pages/about">About</a>
-                <a class="nav-link" href="/pages/contact">Contact</a>
-                <a class="nav-link" href="/komik">Komik</a>
-                <a class="nav-link" href="/orang">Orang</a>
-            </div>
+<nav class="navbar navbar-expand-lg navbar-light" ">
+    <div class=" container">
+    <h3 class="logo">Teknow<span>logue</span></h3>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav style=" color :azure">
+            <a class="nav-link active" href="/">Home</a>
+            <a class="nav-link" href="/pages/about">About</a>
+            <a class="nav-link" href="/pages/contact">Contact</a>
+            <a class="nav-link" href="/komik">Komik</a>
+            <a class="nav-link" href="/orang">Orang</a>
         </div>
+    </div>
     </div>
 </nav>
